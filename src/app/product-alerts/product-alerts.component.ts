6a308e12a550857b42a1ace653ebb565fdfe43b2
oswoo003 <oswoo003@umn.edu>
2019-09-24 11:13:38 -0500

    added listener and action elements to the product-alerts component and it's parent.

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
